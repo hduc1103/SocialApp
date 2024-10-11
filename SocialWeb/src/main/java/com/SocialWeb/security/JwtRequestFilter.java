1929922f03c8ab858e3d6b4681e51a2596c8d3f6
hduc1103 <maihoangduc1103.ptts@gmail.com>
2024-10-11 10:15:50 +0700

    jwt fixed

--- SocialWeb/src/main/java/com/SocialWeb/security/JwtRequestFilter.java
+++ SocialWeb/src/main/java/com/SocialWeb/security/JwtRequestFilter.java
@@ -1,15 +1,10 @@
 package com.SocialWeb.security;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,18 +13,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class JwtRequestFilter extends OncePerRequestFilter implements ApplicationContextAware {
+public class JwtRequestFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
-    private static ApplicationContext applicationContext;
+    private final UserDetail userDetail;
 
-    public JwtRequestFilter(JwtUtil jwtUtil) {
+    public JwtRequestFilter(JwtUtil jwtUtil, UserDetail userDetail) {
         this.jwtUtil = jwtUtil;
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        JwtRequestFilter.applicationContext = context;
+        this.userDetail = userDetail;
     }
 
     @Override
@@ -42,8 +33,7 @@ public class JwtRequestFilter extends OncePerRequestFilter implements Applicatio
             String username = jwtUtil.extractUsername(jwt);
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetailsService userDetailsService = applicationContext.getBean(UserDetailsService.class);
-                var userDetails = userDetailsService.loadUserByUsername(username);
+                var userDetails = userDetail.loadUserByUsername(username);
 
                 if (jwtUtil.validateToken(jwt, userDetails.getUsername())) {
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
